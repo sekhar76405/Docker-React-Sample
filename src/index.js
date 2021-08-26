9320ffe1891d71188f5ea777c19c69866607e7a4
Inder24 <likhariinder@gmail.com>
2021-08-26 16:58:45 +0530

    Sample Update

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello World  , This website is working</h1>;
+ReactDOM.render(element, document.getElementById("root"));
