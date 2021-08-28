f50148309cbee49614fb94ae15dab8f32cab88e6
Inder24 <likhariinder@gmail.com>
2021-08-28 17:46:36 +0530

    Fix build error

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const element = <h1>Hello World  , This website is working</h1>;
-ReactDOM.render(element, document.getElementById("root");
+ReactDOM.render(element, document.getElementById("root"));
